3b423d685f37845c7456fe5d11cd3002a99dc0b6
seu5022 <kdhht5022@gmail.com>
2019-05-31 16:24:46 +0900

    README.md
    README.md

--- 0x200_Programming/7_command_line_arguments.sh
+++ 0x200_Programming/7_command_line_arguments.sh
@@ -15,4 +15,18 @@ gcc -o commandline commandline.c
 # argument #4	-	the
 # argument #5	-	earth!
 
+# Sometimes a program will want to use a command-line argument 
+# as an integer as opposed to a string.
+# `atoi()` which is short for ASCII to integer can be a solution.
+
+gcc convert.c
+./a.out
+# Usage: ./a.out <message> <# of times to repeat>
+./a.out 'Hello, world!' 5
+# Repeating 5 times..
+#   0 - Hello, world!
+#   1 - Hello, world!
+#   2 - Hello, world!
+#   3 - Hello, world!
+#   4 - Hello, world!
 
