3ee768c0014b1fa6cf85c24ae683ee8de6cda3ec
seu5022 <kdhht5022@gmail.com>
2019-06-10 23:35:15 +0900

    README.md
    README.md

--- 0x200_Programming/10_memory_segment_in_c.sh
+++ 0x200_Programming/10_memory_segment_in_c.sh
@@ -15,3 +15,19 @@ gcc -o memory_segment memory_segment.c
 # stack_var is at address 0x5974201c
 # The function's stack_var is at address 0x59741ff4
 
+
+# 0x272 Using the Heap 
+# Allocating memory in the heap is done using the `malloc()` function.
+
+gcc -o heap_example heap_example.c
+./heap_example
+
+#  [+] allocating 50 bytes of memory on the heap for char_ptr
+#char_ptr (0x7fbf55402ab0) --> 'This is memory is located on the heap.'
+#	[+] allocating 12 bytes of memory on the heap for int_ptr
+#int_ptr (0x7fbf55400690) --> 31337
+#	[-] freeing char_ptr's heap memory...
+#	[+] allocating another 15 bytes for char_ptr
+#char_ptr (0x7fbf554006a0) --> 'new memory'
+#	[-] freeing int_ptr's heap memory...
+#	[-] freeing char_ptr's heap memory...
