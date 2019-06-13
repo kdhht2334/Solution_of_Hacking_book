aebf4a72365dcf908cee1b598f76bc967b6f8d73
seu5022 <kdhht5022@gmail.com>
2019-06-14 01:53:39 +0900

    README.md
    README.md

---
+++ 0x200_Programming/11_building_on_basics.sh
@@ -0,0 +1,9 @@
+# 0x280 Building on Basics
+
+# 0x281 File Access
+# There are two primary ways to access files in C: file descriptors and filestreams. 
+# File descriptors use a set of low-level I/O functions, and filestreams are a higher-level form of buffered I/O that is built on the lower-level functions.
+
+# (Something blablabla...)
+gcc -o simplenote simmplenote.c
+./simplenote
