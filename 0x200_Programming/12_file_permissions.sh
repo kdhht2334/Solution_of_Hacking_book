5bfe153e384d7298f00f7ea6beb52ae83dbd7923
seu5022 <kdhht5022@gmail.com>
2019-06-25 14:18:47 +0900

    README.md
    README.md

--- 0x200_Programming/12_file_permissions.sh
+++ 0x200_Programming/12_file_permissions.sh
@@ -37,3 +37,57 @@ id daehakim
 
 id root
 # uid=0(root) gid=0(wheel) groups=0(wheel) (...) (personal secrete)
+
+# This example is how the `chsh` program is able to allow any user to change
+# his/her login shell stored in `/etc/passwd`.
+gcc -o uid_demo uid_demo.c
+ls -l uid_demo
+# -rwxr-xr-x  1 root  staff   8.3K Jun 25 13:47 uid_demo*
+
+./uid_demo
+# Because my user ID is like this: uid=501(daehakim).
+# real uid: 501
+# effective uid: 501
+
+sudo chown root:wheel ./uid_demo
+ls -l uid_demo
+# -rwsr-xr-x  1 root  wheel   8.3K Jun 25 13:47 uid_demo*
+
+sudo chmod u+s ./uid_demo
+ls -l uid_demo
+# -rwsr-xr-x  1 root  staff   8.3K Jun 25 13:47 uid_demo*
+
+./uid_demo
+
+# 0 means that the program can access files as root.
+# real uid: 501
+# effective uid: 0
+
+# This same technique can be used in a multi-user note-taking program.
+# The next example will be a modification of the simplenote program;
+# it will also record the user ID of each note's original author.
+gcc -o notetaker notetaker.c
+sudo chown root:group ./notetaker
+sudo chmod u+s ./notetaker
+# -rwsr-xr-x  1 root  group   8.9K Jun 25 14:12 ./notetaker*
+
+./notetaker "this is a test of multi-user notes"
+# [debug] buffer   @ 0x7f8944c02a90: 'this is a test of multi-user notes'
+# [debug] datafile @ 0x7f8944c00620: '/var/notes'
+# [DEBUG] file descriptor is 3
+# Note has been saved.
+
+ls -l /var/notes
+# -rw-------  1 root  wheel    40B Jun 25 14:13 /var/notes
+
+cat /var/notes
+# cat: /var/notes: Permission denied
+sudo cat /var/notes
+# �
+# this is a test of multi-user notes
+
+sudo hexdump -C /var/notes
+# 00000000  f5 01 00 00 0a 74 68 69  73 20 69 73 20 61 20 74  |.....this is a t|
+# 00000010  65 73 74 20 6f 66 20 6d  75 6c 74 69 2d 75 73 65  |est of multi-use|
+# 00000020  72 20 6e 6f 74 65 73 0a                           |r notes.|
+# 00000028
